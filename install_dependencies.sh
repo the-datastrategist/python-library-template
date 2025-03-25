135d16eeade8c57990226d9ca4d0aa93d36d39a1
Gordon Silvera <gordon.silvera@gmail.com>
2025-03-25 09:58:50 +0100

    Create install_dependencies.sh
    File to install poetry dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,11 @@
+# Add core dependencies
+poetry add pandas numpy scikit-learn
+
+# Add data science dependencies
+poetry add matplotlib seaborn jupyterlab
+
+# Add development dependencies
+poetry add --group dev black isort mypy pytest
+
+# Add pre-commit
+poetry add --group dev pre-commit
